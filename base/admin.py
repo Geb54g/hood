22ae97d9efd54c5398b905d543b4c2042d2824cf
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-06-19 23:55:39 +0300

    ft:models registration

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import Profile, NeighbourHood, Business, Post
-
+from .models import *
 
 admin.site.register(Profile)
-admin.site.register(NeighbourHood)
-admin.site.register(Business)
-admin.site.register(Post)
+admin.site.register(Neighbourhood)
